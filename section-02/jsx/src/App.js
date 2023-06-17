032bb30744d1e128148aa3e31e13ac1b2ff3d708
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-06-17 21:13:17 +0000

    refactor(section-02): move app component into its own file

---
+++ section-02/jsx/src/App.js
@@ -0,0 +1,7 @@
+// 1) Create a react component
+function App() {
+    return <h1>Hi there!</h1>;
+}
+
+export default App;
+  
--- section-02/jsx/src/index.js
+++ section-02/jsx/src/index.js
@@ -1,6 +1,7 @@
 // 1) Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2) Get a reference to the div with id of 'root'
 const divRoot = document.getElementById('root');
@@ -8,11 +9,6 @@ const divRoot = document.getElementById('root');
 // 3) Tell react to take control of that element
 const root = ReactDOM.createRoot(divRoot);
 
-// 4) Create a react component
-function App() {
-  return <h1>{new Date().toLocaleTimeString()}</h1>;
-}
-
-// 5) Show the react component on the screen
+// 4) Show the react component on the screen
 root.render(<App />);
 // root.render(React.createElement("h1", null, "Hi there 2!"))
