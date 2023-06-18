a4b6c1d765e271f2f64a35045c5b255c1ae5c8e0
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-06-17 21:20:50 -0300

    feat(section-03): style cards with bulma

--- section-03/pdas/src/App.js
+++ section-03/pdas/src/App.js
@@ -10,10 +10,42 @@ import SiriImage from "./images/siri.png";
 function App () {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard 
+                title="Alexa" 
+                handle="@alexa99" 
+                image={AlexaImage} 
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard 
+                title="Cortana" 
+                handle="@cortana32" 
+                image={CortanaImage} 
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard 
+                title="Siri" 
+                handle="@siri01" 
+                image={SiriImage} 
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
