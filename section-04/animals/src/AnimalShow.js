c3aca9803a4f14eb4bbd08d17a4c46e75324c492
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-06-18 17:55:37 -0300

    feat(section-04): load and show animals svg

--- section-04/animals/src/AnimalShow.js
+++ section-04/animals/src/AnimalShow.js
@@ -1,6 +1,25 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+// import heart from './svg/heart.svg';
+
+const svgAnimalMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse
+};
+
 function AnimalShow({ type }) {
   return (
-    <div>{type}</div>
+    <div>
+      <img alt={type} src={svgAnimalMap[type]} />
+    </div>
   );
 }
 
