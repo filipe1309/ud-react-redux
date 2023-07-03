1d867a1f3a819a8abb857ea524b7488d773a6b9e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-03 15:01:01 -0300

    feat(section-08): add simple context example

--- section-08/books/src/components/BookList.js
+++ section-08/books/src/components/BookList.js
@@ -1,6 +1,9 @@
+import { useContext } from "react";
+import BookContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onDeleteBookById, onEditBookById }) {
+  const { count, incrementCount } = useContext(BookContext);
   const renderedBooks = books.map(book => {
     return <BookShow 
       key={book.id} 
@@ -10,7 +13,11 @@ function BookList({ books, onDeleteBookById, onEditBookById }) {
     />;
   });
 
-  return <div className="book-list" >{renderedBooks}</div>;
+  return <div className="book-list" >
+    <h2>{count}</h2>
+    <button onClick={incrementCount}>Increment</button>
+    {renderedBooks}
+  </div>;
 }
 
 export default BookList;
