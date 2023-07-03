bddac90c247e0202adb433d82172a76c768c8054
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-03 15:56:05 -0300

    refactor: remove context example

--- section-08/books/src/components/BookList.js
+++ section-08/books/src/components/BookList.js
@@ -1,9 +1,6 @@
-import { useContext } from "react";
-import BookContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onDeleteBookById, onEditBookById }) {
-  const { count, incrementCount } = useContext(BookContext);
   const renderedBooks = books.map(book => {
     return <BookShow 
       key={book.id} 
@@ -14,8 +11,6 @@ function BookList({ books, onDeleteBookById, onEditBookById }) {
   });
 
   return <div className="book-list" >
-    <h2>{count}</h2>
-    <button onClick={incrementCount}>Increment</button>
     {renderedBooks}
   </div>;
 }
--- section-08/books/src/context/books.js
+++ section-08/books/src/context/books.js
@@ -1,17 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext } from "react";
 
 const BooksContext = createContext();
 
 function Provider({ children }) {
-  const [count, setCount] = useState(5);
-
-  const valueToShare = {
-    count,
-    incrementCount: () => setCount(count + 1)
-  };
-
+  
   return (
-    <BooksContext.Provider value={valueToShare}>
+    <BooksContext.Provider value={{}}>
       {children}
     </BooksContext.Provider>
   );
