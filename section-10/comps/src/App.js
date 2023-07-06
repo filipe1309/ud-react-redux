bf42c34d740310a70a06ff36300350bb864f0df2
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-06 19:49:50 -0300

    feat(section-10): validate variation button props with prop-types

--- section-10/comps/src/App.js
+++ section-10/comps/src/App.js
@@ -3,7 +3,7 @@ import Button from './/Button';
 function App() {
   return (
     <div>
-      <div><Button>Click me 1!</Button></div>
+      <div><Button success >Click me 1!</Button></div>
       <div><Button>Click me 2!</Button></div>
       <div><Button>Click me 3!</Button></div>
       <div><Button>Click me 4!</Button></div>
--- section-10/comps/src/Button.js
+++ section-10/comps/src/Button.js
@@ -1,5 +1,21 @@
+import PropTypes from "prop-types";
+
 function Button({ children }) {
   return <button>{children}</button>;
 }
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count = Number(!!primary) + Number(!!secondary) + Number(!!success) 
+      + Number(!!warning) + Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        'Only one variation (primary, secondary, success, warning, danger) can be used at a time.'
+      );
+    }
+  }
+};
 
 export default Button;
