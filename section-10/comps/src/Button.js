c60502c33197c79c689c1f85c0c7aa74f18dbea7
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-08 14:05:51 -0300

    feat(section-10): customize button

--- section-10/comps/src/Button.js
+++ section-10/comps/src/Button.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 function Button({ children }) {
-  return <button>{children}</button>;
+  return <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">{children}</button>;
 }
 
 Button.propTypes = {
