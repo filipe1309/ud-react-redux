443f94ec3ffee98a69f5dd0c830465291f5b492d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-09 14:14:35 -0300

    feat(section-11): init section

---
+++ section-11/comps/src/App.js
@@ -0,0 +1,40 @@
+import { GoBell, GoCloud, GoDatabase } from 'react-icons/go';
+import Button from './Button';
+
+function App () {
+
+  const handleClick = () => {
+    console.log('clicked');
+  }
+
+  const handleMouseEnter = () => {
+    console.log('mouse entered');
+  }
+
+  return (
+    <div>
+      <div>
+        <Button primary rounded outline onClick={handleClick} className="mb-5" >
+          <GoBell />
+          Primary
+        </Button>
+      </div>
+      <div>
+        <Button secondary onMouseEnter={handleMouseEnter} >
+          <GoCloud />
+          Secondary
+        </Button>
+      </div>
+      <div>
+        <Button success outline>
+          <GoDatabase />
+          Success
+        </Button>
+      </div>
+      <div><Button warning>Warning</Button></div>
+      <div><Button danger rounded>Danger</Button></div>
+    </div>
+  );
+}
+
+export default App;
