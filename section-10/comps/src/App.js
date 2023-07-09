ae2f16de456eafb61cbea850c71b5590fc74c9f2
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-08 22:30:05 -0300

    feat(section-10): add icons with react-icons

--- section-10/comps/src/App.js
+++ section-10/comps/src/App.js
@@ -1,11 +1,27 @@
+import { GoBell, GoCloud, GoDatabase } from 'react-icons/go';
 import Button from './/Button';
 
 function App() {
   return (
     <div>
-      <div><Button primary rounded outline>Primary</Button></div>
-      <div><Button secondary>Secondary</Button></div>
-      <div><Button success outline>Success</Button></div>
+      <div>
+        <Button primary rounded outline>
+        <GoBell />
+        Primary
+        </Button>
+      </div>
+      <div>
+        <Button secondary>
+          <GoCloud />
+          Secondary
+        </Button>
+      </div>
+      <div>
+        <Button success outline>
+          <GoDatabase />
+          Success
+        </Button>
+      </div>
       <div><Button warning>Warning</Button></div>
       <div><Button danger rounded>Danger</Button></div>
     </div>
