3e8acccd9050aad0bbc72887437caccd2265c30b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-11 19:29:41 -0300

    feat(section-11): add click event handler to expand items of accordion

--- section-11/comps/src/components/Accordion.js
+++ section-11/comps/src/components/Accordion.js
@@ -3,11 +3,14 @@ import { useState } from "react";
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
+  const handleClick = (index) => {
+    setExpandedIndex(index);
+  }
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
-
     return <div key={item.id} >
-      <div>{item.label}</div>
+      <div onClick={() => handleClick(index)}>{item.label}</div>
       {isExpanded && <div>{item.content}</div>}
     </div>;
   });
