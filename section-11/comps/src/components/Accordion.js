eaad88b6cba6d949af0b6949d683dc5b293b4a19
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-12 12:43:52 -0300

    feat(section-11): displaying icons of accordion

--- section-11/comps/src/components/Accordion.js
+++ section-11/comps/src/components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go"
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
@@ -9,12 +10,20 @@ function Accordion({ items }) {
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
-    return <div key={item.id} >
-      <div onClick={() => handleClick(index)}>{item.label}</div>
-      {isExpanded && <div>{item.content}</div>}
-    </div>;
+    const icon = <span>
+      {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+    </span>
+    return (
+      <div key={item.id} >
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    );
   });
-  
+
   return <div>{renderedItems}</div>;
 }
 
